cfcbb32a56437c71576ab4793c941cbfd2811280
mzhuravlev <mzhuravlev@diasoft.ru>
2026-01-12 10:51:11 +0500

    Added getAllUsersSortByDescTest

--- src/test/java/tests/junit5/api/SimpleApiTest.java
+++ src/test/java/tests/junit5/api/SimpleApiTest.java
@@ -1,17 +1,20 @@
 package tests.junit5.api;
 
-import io.restassured.http.ContentType;
-import org.hamcrest.text.MatchesPattern;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.Matchers.*;
 
 public class SimpleApiTest {
 
     @Test
-    public void getAllUserTest(){
+    public void getAllUserTest() {
         given().get("https://fakestoreapi.com/users")
                 .then()
                 .log().all()
@@ -19,12 +22,46 @@ public class SimpleApiTest {
     }
 
     @Test
-    public void getSingleUserTest(){
+    public void getSingleUserTest() {
         int userId = 2;
         given().pathParam("userId", userId)
                 .get("https://fakestoreapi.com/users/{userId}")
                 .then().log().all()
                 .body("id", equalTo(userId))
                 .body("address.zipcode", matchesPattern("\\d{5}-\\d{4}"));
+    }
+
+    @Test
+    public void getAllUsersWithLimitTest() {
+        int limitSize = 3;
+        given().queryParam("limit", limitSize)
+                .get("https://fakestoreapi.com/users/")
+                .then().log().all()
+                .statusCode(200)
+                .body("", hasSize(greaterThanOrEqualTo(limitSize)));
+    }
+
+    @Test
+    public void getAllUsersSortByDescTest() {
+        String sortType = "desc";
+        Response sortedResponse = given().queryParam("sort", sortType)
+                .get("https://fakestoreapi.com/users/")
+                .then().log().all()
+                .extract().response();
+
+        Response notSortedResponse = given()
+                .get("https://fakestoreapi.com/users/")
+                .then().log().all().extract().response();
+
+        List<Integer> sortedResponseIds = sortedResponse.jsonPath().getList("id");
+        List<Integer> notSortedResponseIds = notSortedResponse.jsonPath().getList("id");
+
+        List<Integer> sortedByCode = notSortedResponseIds
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        Assertions.assertNotEquals(sortedResponseIds, notSortedResponseIds);
+        Assertions.assertEquals(sortedByCode, sortedResponseIds);
     }
 }
